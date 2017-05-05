7ddc77abf8b980221298370b6c618278bedeef60
Ruben Das <rubda680@student.liu.se>
2017-05-05 14:40:21 +0200

    Testing out React, first draft of api calls and use of it.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { Container } from 'reactstrap';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class Applicants extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: [],
+            repos: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get(`https://api.github.com/users/${this.props.github}/repos`)
+            .then(res => {
+                const repos = res.data.map(obj => obj);
+                this.setState({ repos });
+            });
+
+
+        axios.get(`http://127.0.0.1:3000/api/users`)
+            .then(res => {
+                const users = res.data.map(obj => obj);
+                console.log(users);
+                this.setState({ users });
+            });
+
+    }
+
+    render() {
+        return (
+            <Container>
+                <div>
+                    <br/>
+                    <h1>Applications</h1>
+                    <br/>
+                    <h3>Ruben Das</h3>
+                    <h6>GitHub Repos</h6>
+                    <ul>
+                        {this.state.repos.map(repos =>
+                            <li key={ repos.id }><a href={ repos.html_url }>{ repos.name } - { repos.description }</a></li>
+                        )}
+                    </ul>
+                </div>
+            </Container>
+        );
+    }
 }
 
-export default App;
+ReactDOM.render(
+    <Applicants github="rubda"/>,
+    document.getElementById('root')
+);
+
+export default Applicants;
