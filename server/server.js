a5ab5551c9982a8c2e450a25207e43261a31435e
Ruben Das <rubda680@student.liu.se>
2017-05-05 11:33:54 +0200

    Revert "Removing Access-Control stuff for now."
    This reverts commit 81091b01ebe0fda8c169813c9fd04fdf8ff92d0d.

--- server/server.js
+++ server/server.js
@@ -11,9 +11,16 @@ const app = express();
 const dbHost = 'mongodb://database/TDDD272017_applicants';
 mongoose.connect(dbHost);
 
+
 // Get POST stuff to work
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+});
 
 app.use('/api', users);
 
